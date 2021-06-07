04d16e7c4bda0a3fb0ae585295ee9456fe10ab08
learncode2021 <jsowaruk@gmail.com>
2021-06-07 19:18:44 +0200

    html and javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+//كتابة كود html داخل جافا سكريبت:
+
+document.write('<h1>bage one</h1>');
+
+document.querySelector('h1').style.color='red'
+"red"
+
+
+document.querySelector('h1').style.fontSize = '60px'
+"60px"
+
+
+document.querySelector('h1').style.background= 'blue'
+"blue"
